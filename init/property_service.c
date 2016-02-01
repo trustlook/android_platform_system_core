194bef6a39a9a8a260aea24c4cd9f16b784c377e
Jong Kook An <jan@trustlook.com>
2016-02-01 14:28:34 -0800

    initial Trustlook implementation commit

--- init/property_service.c
+++ init/property_service.c
@@ -94,6 +94,7 @@ struct {
     { "persist.security.", AID_SYSTEM,   0 },
     { "persist.service.bdroid.", AID_BLUETOOTH,   0 },
     { "selinux."         , AID_SYSTEM,   0 },
+    { "taint.",            AID_SYSTEM,   0 },
     { NULL, 0, 0 }
 };
 
